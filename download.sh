6c5ea5a9761c256cb430c30557dcdcd999f9505b
Imam Dwicaksono <imam.dwicaksono@mmsgroup.co.id>
2025-08-14 15:13:53 +0700

    feat: update download script to include timestamped log files and process management

--- download.sh
+++ download.sh
@@ -1,1 +1,8 @@
-nohup ./run.sh > download_output.log 2>&1 &
+#!/bin/bash
+
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+PID=$$
+LOG_FILE="download_logs/download_output_${TIMESTAMP}_${PID}.log"
+
+nohup ./run.sh > "$LOG_FILE" 2>&1 &
+echo "Download berjalan di background (PID $!). Log: $LOG_FILE"
